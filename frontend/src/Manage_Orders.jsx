99dd7f11d5590c73b98678ce8563c16310c4c3a2
Gagan-K916 <gagan.kulal916@gmail.com>
2026-04-15 12:02:15 +0530

    Done

--- frontend/src/Manage_Orders.jsx
+++ frontend/src/Manage_Orders.jsx
@@ -76,7 +76,7 @@ const cancelOrder = async (id) => {
       {ord.products.map((p, i) => (
         <div key={i} className="product-row">
        <img
-  src={`https://res.cloudinary.com/process.env.CLOUD_NAME/Decathlon/${p.productId?.file}`}
+  src={p.productId?.file} 
 />
           <div className="product-info">
             <h4> {p.productId?.productName || p.productId?.name || "No Name"}</h4>
