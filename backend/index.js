17e78a2fb4140e317e7403de22224824de297f88
Gagan-K916 <gagan.kulal916@gmail.com>
2026-04-16 10:13:58 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -420,11 +420,11 @@ app.post('/orders', authMiddleware, async (req, res) => {
     });
     await order.save();
     const user = await UserModel.findById(req.user.id);
-    const order1=await OrderModel.findById(req.user.order.id);
-    const product=await ProductModel.findById(req.product.id);
+   // const order1=await OrderModel.findById(req.user.order.id);
+   // const product=await ProductModel.findById(req.product.id);
     
 await sendOrderEmail(user.email, "PLACED", order._id);
-await GenerateBill(user.email, "PLACED", order1.orderId,product.productName,product.productPrice,order.quantity, paymentId,totalAmount);
+//await GenerateBill(user.email, "PLACED", order1.orderId,product.productName,product.productPrice,order.quantity, paymentId,totalAmount);
     await CartModel.updateOne(
       { userId: req.user.id },
       { $set: { items: [] } }
